0c82d7796b76135b1bede5d8bf7d01dfd8bd7ac0
Ennes <nathan.soulier26@gmail.com>
2020-11-02 16:04:41 +0100

    Carte & CarteAbonnement & Genre

--- Model/Carte.java
+++ Model/Carte.java
@@ -1,5 +1,31 @@
 package Model;
 
 public abstract class Carte {
+	private int noCarte;
+	private String libelle;
+	
+	public Carte(int noCarte, String libelle) {
+		this.noCarte = noCarte;
+		this.libelle = libelle;
+	}
+	
+	public int getPrixParJour() {
+		return -1;
+	}
 
+	public int getNoCarte() {
+		return noCarte;
+	}
+
+	public void setNoCarte(int noCarte) {
+		this.noCarte = noCarte;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
 }
