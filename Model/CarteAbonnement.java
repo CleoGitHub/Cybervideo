8613b9af1881884556ebd6884b0165f28a022e97
Ennes <nathan.soulier26@gmail.com>
2020-11-02 16:11:21 +0100

    CarteAbonnement & Genre toString

--- Model/CarteAbonnement.java
+++ Model/CarteAbonnement.java
@@ -1,16 +1,26 @@
 package Model;
 import java.time.*;
+import java.util.ArrayList;
 
 public class CarteAbonnement extends Carte {
 	private static final int prixParJour = 5;
 	
 	private LocalDate dateIns;
 	private int solde;
+	private ArrayList<Genre> genresInterdits = new ArrayList<Genre>();
+	private ArrayList<Location> locations = new ArrayList<Location>();
 	
 	public CarteAbonnement(LocalDate dateIns, int noCarte, String libelle) {
 		super(noCarte, libelle);
 		this.setDateIns(dateIns);
 		this.setSolde(0);
+	}
+	
+	public CarteAbonnement(LocalDate dateIns, int noCarte, String libelle, ArrayList<Genre> genreInterdits) {
+		super(noCarte, libelle);
+		this.setDateIns(dateIns);
+		this.setSolde(0);
+		this.genresInterdits = genreInterdits;
 	}
 	
 	@Override
@@ -32,5 +42,37 @@ public class CarteAbonnement extends Carte {
 
 	public void setDateIns(LocalDate dateIns) {
 		this.dateIns = dateIns;
+	}
+
+	public ArrayList<Genre> getGenresInterdits() {
+		return genresInterdits;
+	}
+
+	public void ajouterGenreInterdit(Genre g) {
+		genresInterdits.add(g);
+	}
+
+	public void supprimerGenreInterdit(Genre g) {
+		genresInterdits.remove(g);
+	}
+	
+	public void supprimerGenreInterdit(int i) {
+		genresInterdits.remove(i);
+	}
+	
+	public ArrayList<Location> getLocations() {
+		return locations;
+	}
+
+	public void ajouterLocation(Location l) {
+		locations.add(l);
+	}
+	
+	public void supprimerLocation(Location l) {
+		locations.remove(l);
+	}
+	
+	public void supprimerLocation(int i) {
+		locations.remove(i);
 	}
 }
