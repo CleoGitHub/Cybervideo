2d5d72308cd9e3f7ce741728b0dfd27674389710
CleoGitHub <survcleo@hotmail.fr>
2020-11-02 16:42:19 +0100

    ajout des methodes attribut de techicien

--- Model/Technicien.java
+++ Model/Technicien.java
@@ -1,5 +1,38 @@
 package Model;
 
-public class Technicien {
+import java.util.ArrayList;
 
+public class Technicien {
+	private String nom;
+	private String password;
+	private ArrayList<Location> listeLocations;
+	
+	public Technicien(String nom, String password) {
+		this.nom = nom;
+		this.password = password;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public ArrayList<Location> getListeLocations() {
+		return listeLocations;
+	}
+	
+	public void setListeLocations(ArrayList<Location> listeLocations) {
+		this.listeLocations = listeLocations;
+	}	
 }
