eac7ef9a9747ad12d6b6c640eb4c8abee6fec42d
troubadourjimmy <42512517+troubadourjimmy@users.noreply.github.com>
2020-11-08 23:07:04 +0100

    Update Panier.java

--- Model/Panier.java
+++ Model/Panier.java
@@ -1,5 +1,57 @@
 package Model;
 
-public class Panier {
+public class Panier
+{
+  private ArrayList<Location> dvds;//des dvds que l'on ajoute au fur et a mesure de l'utilisation de l'application	 
+	private Carte carte;		
+			
+	public Panier()
+	{
+		this.dvds = new ArrayList<Location>();
+		 
+	}
+			
+	public ArrayList<Location> getDvds() 
+	{
+		return dvds;
+	}
+	public void setDvds(ArrayList<Location> dvds) 
+	{
+		this.dvds = dvds;
+	}
 
+
+	public void ajouter(Location dvd)
+	{
+		this.dvds.add(dvd);
+	}
+	
+	public void retirer(Location dvd)
+	{
+		this.dvds.remove(dvd);
+	}
+	
+	public ArrayList<Location> payer(Carte c)
+	{
+		this.carte = c;
+		double Prixtotal = 0;
+		for(int i=0; i<getDvds().size();i++ )
+		{
+			Location dvd = dvds.get(i);
+			c.getLocationsEnCours().add(dvd);
+			Prixtotal = Prixtotal + dvd.calculerPrix();			
+		}
+		
+		//juger le type de carte, carte bancaire ou carte abonnement?????
+		if (c instanceof CarteAbonnement ==true)
+		{
+			 
+		}
+		
+		return c.getLocationsEnCours();
+		
+		
+		
+		 
+	}
 }
