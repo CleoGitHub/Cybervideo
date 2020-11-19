d0ff8d7aa96a5df143324576b0b51c5a84b03e66
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2020-11-19 13:14:27 +0100

    ajouter une classe controller, utilise le model qui metAjoure() les vues

---
+++ Controller/Controller.java
@@ -0,0 +1,24 @@
+package Controller;
+
+import Model.CyberVideo;
+
+import javax.swing.*;
+import java.util.List;
+
+public class Controller {
+    private CyberVideo model;
+    private List<JPanel> vuesPile;
+    private JLayeredPane contenuPane;
+
+    public Controller(CyberVideo model) {
+        this.model = model;
+        // TODO: creat panels
+    }
+
+    // TODO: void prev(): changer la vue du contenuPane à la précedenté
+
+    // TODO: suivant(): changer la vue
+
+    // TODO: actions utilisant le model
+
+}
