db54df978d84f553193746dc707bbc884f9aa754
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2020-11-19 13:17:35 +0100

    le Main du projet

---
+++ CyberVideoGUI.java
@@ -0,0 +1,19 @@
+import Model.CyberVideo;
+
+import javax.swing.*;
+import java.util.List;
+
+public class CyberVideoGUI implements Runnable {
+    private CyberVideo model;
+    private List<JPanel> vuesPile;
+    private JLayeredPane contenuPane;
+
+    public CyberVideoGUI() {
+        // TODO: créer controller, et model
+    }
+
+    @Override
+    public void run() {
+        // TODO: créer/excuter Jframe
+    }
+}
