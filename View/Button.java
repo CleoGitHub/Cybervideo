5336685d0886c38511f339bf538d2f92a54aecdb
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2020-11-19 10:48:45 +0100

    modifier le code do Button

--- View/Button.java
+++ View/Button.java
@@ -7,46 +7,25 @@ import java.awt.Dimension;
 public class Button extends JLabel {
 
     Button(String imagePath, String text, int largeur, int hauteur) {
-
-        super();
-
+        this(imagePath);
         this.setText(text);
         this.setHorizontalTextPosition(JLabel.CENTER);
-
-        try {
-            this.setIcon(new ImageIcon(imagePath));
-        } catch (Exception e) {
-            System.err.println("can't read image");
-        }
-
         this.setPreferredSize(new Dimension(largeur, hauteur));
     }
 
     Button(String imagePath, String text) {
-
-        super();
-
+        this(imagePath);
         this.setForeground(java.awt.Color.WHITE);
         this.setText(text);
         this.setHorizontalTextPosition(JLabel.CENTER);
-
-        try {
-                this.setIcon(new ImageIcon(imagePath));
-            } catch (Exception e) {
-                System.err.println("can't read image");
-            }
-
     }
 
     Button(String imagePath) {
-
         super();
-
         try {
-                this.setIcon(new ImageIcon(imagePath));
-            } catch (Exception e) {
-                System.err.println("can't read image");
-            }
-
+            this.setIcon(new ImageIcon(imagePath));
+        } catch (Exception e) {
+            System.err.println("can't read image, " + e.getMessage());
+        }
     }
 }
