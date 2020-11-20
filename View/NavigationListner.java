e351383e983ba1f2fe6cc4513240b202ee5bb2b8
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2020-11-20 20:52:40 +0100

    ajouter un Listner pour les Buttons de navigation

---
+++ View/NavigationListner.java
@@ -0,0 +1,56 @@
+package View;
+
+import Controller.Controller;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class NavigationListner extends MouseAdapter {
+    static final int PREC = 0;
+    static final int TECHNICIEN = 1;
+
+    private Controller controller;
+
+    public NavigationListner(Controller controller) {
+        super();
+        this.controller = controller;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int action = ((Button)e.getSource()).getId();
+        switch (action) {
+            case PREC:
+                controller.vuePrec();
+                break;
+
+            case TECHNICIEN:
+                controller.vueSuiv(controller.getVueTechnecien());
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
