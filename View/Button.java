9d29211e50b7af3f2f21ca22aff892b1473ac16f
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2020-11-22 16:20:13 +0100

    ajouter la capabilitée de créer une Button vide

--- View/Button.java
+++ View/Button.java
@@ -8,29 +8,33 @@ public class Button extends JLabel {
 
     private int id;
 
-    Button(String imagePath, String text, int largeur, int hauteur) {
+    public Button(String imagePath, String text, int largeur, int hauteur) {
         this(imagePath);
         this.setText(text);
         this.setHorizontalTextPosition(JLabel.CENTER);
         this.setPreferredSize(new Dimension(largeur, hauteur));
     }
 
-    Button(String imagePath, String text) {
+    public Button(String imagePath, String text) {
         this(imagePath);
         this.setForeground(java.awt.Color.WHITE);
         this.setText(text);
         this.setHorizontalTextPosition(JLabel.CENTER);
     }
 
-    Button(String imagePath) {
-        super();
-        id = 0;
+    public Button(String imagePath) {
+        this();
         setForeground(Color.WHITE);
         try {
             this.setIcon(new ImageIcon(imagePath));
         } catch (Exception e) {
             System.err.println("can't read image, " + e.getMessage());
         }
+    }
+
+    public Button() {
+        super();
+        id = 0;
     }
 
 
